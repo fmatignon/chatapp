d7024d050531ef343e806a7f9672a699ff4530ad
federmozzon <mozzonfederico@gmail.com>
2022-03-08 15:22:49 -0300

    Devuelve un usuario, codificado a jwt

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
 const userController = require('../controllers/user.controller');
 const User = require('../models/user.model');
 
@@ -23,41 +25,35 @@ router.post('/signup', async (req, res) => {
   });
   console.log(await checkMail(req.body.mail))
   if(await checkMail(req.body.mail)){
-    const savedUser = await user.save();
-    try {
-      res.json(savedUser);
-    } catch(err) {
-      res.json({message: err});
-    }
+    let savedUser = await user.save();
+    try {res.json(savedUser);} 
+    catch(err) {res.json({message: err});}
   }
   else res.status(409).send({message:"Mail alredy exist"})
 });
 
 router.put('/', async (req, res) => {
   const savedUser = await User.findOneAndUpdate({_id: req.body._id}, req.body);
-  try {
-    res.json(savedUser);
-  } catch(err) {
-    res.json({message: err});
-  }
+  try { res.json(savedUser);} 
+  catch(err) { res.json({message: err});}
 });
 
 // iniciar sesión, falta hashear la contraseńa
 // TODO: agregar JWT. Comprobar contraseña hasheada.
 router.post('/login', async (req, res) => {
   const currentUser = await User.findOne({username: req.body.username}).exec()
-  if((currentUser != null)&&(req.body.password == currentUser.password)) try {res.json(currentUser)} catch(err) {res.json({message: err})}
+  // Es una implementación precaria de jwt, se puede complejizar
+  const token = jwt.sign({username:currentUser.username},'shhhhh');
+  if((currentUser != null)&&(req.body.password == currentUser.password)) {
+    try {res.json(token)} 
+    catch(err) {res.json({message: err})}}
   else res.status(403).send({message:"wrong password or username"})
 })
 
 router.post('/delete', async (req, res) => {
-  console.log(req.body)
-  var cosito = await User.findOneAndDelete({mail:req.body.mail}).exec();
-  try {
-    res.json({message:"User with mail "+req.body.mail+" was deleted"});
-  } catch(err) {
-    res.json({message: err});
-  }
+  await User.findOneAndDelete({mail:req.body.mail}).exec();
+  try {res.json({message:"User with mail "+req.body.mail+" was deleted"});} 
+  catch(err) {res.json({message: err});}
 })
 
 
